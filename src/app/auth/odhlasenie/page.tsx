8e66d32d90191cbd02f7ea465252efccdbaf2994
PeterMego17 <peter.mego17@gmail.com>
2024-11-22 09:53:45 +0100

    prihlasovanie

--- src/app/auth/odhlasenie/page.tsx
+++ src/app/auth/odhlasenie/page.tsx
@@ -1,15 +1,18 @@
-// src/app/auth/Odhlasenie/page.tsx
+'use client';
 
-
+import { signOut } from "next-auth/react";
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 export const metadata = { title: "Odhlásenie | ZoškaSnap" };
 
 export default function SignOut() {
-
   return (
-
-      <Typography> Odhlásenie </Typography>
-
+    <div>
+      <Typography variant="h4">Odhlásenie</Typography>
+      <Button variant="contained" onClick={() => signOut()}>
+        Odhlásiť sa
+      </Button>
+    </div>
   );
 }
--- src/app/auth/prihlasenie/page.tsx
+++ src/app/auth/prihlasenie/page.tsx
@@ -1,15 +1,16 @@
-// src/app/auth/Prihlasenie/page.tsx
+'use client';
 
-
+import { signIn } from "next-auth/react";
 import Typography from '@mui/material/Typography';
-
-export const metadata = { title: "Prihlásenie | ZoškaSnap" };
+import Button from '@mui/material/Button';
 
 export default function SignIn() {
-
   return (
-
-      <Typography> Prihlásenie </Typography>
-
+    <div>
+      <Typography variant="h4">Prihlásenie</Typography>
+      <Button variant="contained" onClick={() => signIn("google")}>
+        Prihlásiť sa cez Google
+      </Button>
+    </div>
   );
 }
